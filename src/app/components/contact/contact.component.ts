9f7d4751d05002455bad6c95401083a1374a8116
Barbara Coelho <balicoelho@gmail.com>
2023-06-29 12:59:29 -0300

    Inclusão da validação de form e outros ajustes

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -1,8 +1,26 @@
 import { Component } from '@angular/core';
+import { NgModel } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
   styleUrls: ['./contact.component.css'],
 })
-export class ContactComponent {}
+export class ContactComponent {
+  tamMax: boolean = false;
+  onSubmit(form: any) {
+    const submission = form.value;
+    localStorage.setItem('cadastro', JSON.stringify(submission));
+    alert('Obrigada pelo contato! Informações enviadas!');
+    form.reset();
+  }
+
+  checkMaxLength(nameControl: NgModel) {
+    if (nameControl.value.length == '300') {
+      this.tamMax = true;
+    }
+    if (nameControl.value.length < '300') {
+      this.tamMax = false;
+    }
+  }
+}
